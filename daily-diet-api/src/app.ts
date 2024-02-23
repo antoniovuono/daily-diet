96547173053c7c5b60f8c85c121f49ec0fb0accc
Antonio Vuono <antoniosvuono@icloud.com>
2024-02-23 16:18:37 -0300

    refactor: adjusts from meal metrics routes

--- daily-diet-api/src/app.ts
+++ daily-diet-api/src/app.ts
@@ -3,7 +3,6 @@ import { usersRoutes } from './routes/users'
 import { authRoutes } from './routes/auth'
 import { mealsRoutes } from './routes/meals'
 import { userAuthenticated } from './middleware/userAuthenticated'
-import { mealMetrics } from './routes/mealMetrics'
 
 export const app = Fastify()
 
@@ -19,14 +18,6 @@ app.register(async (mealsApp) => {
   mealsApp
     .register(mealsRoutes, {
       prefix: '/meals',
-    })
-    .addHook('preHandler', userAuthenticated)
-})
-
-app.register(async (mealsApp) => {
-  mealsApp
-    .register(mealMetrics, {
-      prefix: '/meals-metrics',
     })
     .addHook('preHandler', userAuthenticated)
 })
--- daily-diet-api/src/routes/meals.ts
+++ daily-diet-api/src/routes/meals.ts
@@ -111,4 +111,28 @@ export const mealsRoutes = async (app: FastifyInstance) => {
         .send({ message: 'Failure to communicate with the database!"', err })
     }
   })
+
+  app.get(
+    '/metrics',
+    async (
+      request: FastifyRequest & { user?: string },
+      reply: FastifyReply,
+    ) => {
+      try {
+        const meals = await knex('meals')
+          .where({ user_id: request.user })
+          .select('*')
+
+        return reply.status(201).send({
+          totalMeals: meals.length,
+          totalDietMeals: meals.filter((meal) => meal.diet_meal).length,
+          totalNonDietMeals: meals.filter((meal) => !meal.diet_meal).length,
+        })
+      } catch (err) {
+        return reply
+          .status(500)
+          .send({ message: 'Failure to communicate with the database!"', err })
+      }
+    },
+  )
 }
